47b29a5c91469283c4fb4d55cc78e60218b42130
jonemos <jonemos7788@gmail.com>
2025-08-08 16:59:33 +0900

    ㅁㅁㅁ

--- src/components/editor/panels/LibraryPanel.jsx
+++ src/components/editor/panels/LibraryPanel.jsx
@@ -251,14 +251,14 @@ const LibraryPanel = ({ onObjectDrop, onClose, forceRefresh = 0 }) => {
             {objectLibrary.map((object) => (
               <div
                 key={object.id}
-                className="object-item"
+                className="library-item"
                 draggable
                 onDragStart={(e) => handleDragStart(e, object)}
                 onDragEnd={handleDragEnd}
                 onClick={() => handleClick(object)}
                 title={object.name}
               >
-                <div className="object-thumbnail">
+                <div className="library-thumbnail">
                   <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
                     {object.id === 'cube' && (
                       <path d="M21,16.5C21,16.88 20.79,17.21 20.47,17.38L12.57,21.82C12.41,21.94 12.21,22 12,22C11.79,22 11.59,21.94 11.43,21.82L3.53,17.38C3.21,17.21 3,16.88 3,16.5V7.5C3,7.12 3.21,6.79 3.53,6.62L11.43,2.18C11.59,2.06 11.79,2 12,2C12.21,2 12.41,2.06 12.57,2.18L20.47,6.62C20.79,6.79 21,7.12 21,7.5V16.5M12,4.15L6.04,7.5L12,10.85L17.96,7.5L12,4.15M5,15.91L11,19.29V12.58L5,9.21V15.91M19,15.91V9.21L13,12.58V19.29L19,15.91Z"/>
@@ -296,14 +296,14 @@ const LibraryPanel = ({ onObjectDrop, onClose, forceRefresh = 0 }) => {
               {libraryMeshes.map((object) => (
                 <div
                   key={object.id}
-                  className="object-item library-mesh"
+                  className="library-item library-mesh"
                   draggable
                   onDragStart={(e) => handleDragStart(e, object)}
                   onDragEnd={handleDragEnd}
                   onClick={() => handleClick(object)}
                   title={`${object.name} (라이브러리)`}
                 >
-                  <div className="object-thumbnail">
+                  <div className="library-thumbnail">
                     {object.isLoadingThumbnail ? (
                       <div className="thumbnail-loading">
                         <div className="loading-spinner"></div>
@@ -337,14 +337,14 @@ const LibraryPanel = ({ onObjectDrop, onClose, forceRefresh = 0 }) => {
               customMeshes.map((mesh) => (
                 <div
                   key={mesh.id}
-                  className="object-item custom-mesh"
+                  className="library-item custom-mesh"
                   draggable
                   onDragStart={(e) => handleDragStart(e, mesh)}
                   onDragEnd={handleDragEnd}
                   onClick={() => handleClick(mesh)}
                   title={mesh.name}
                 >
-                  <div className="object-thumbnail">
+                  <div className="library-thumbnail">
                     <img 
                       src={mesh.thumbnail} 
                       alt={mesh.name}
@@ -397,14 +397,14 @@ const LibraryPanel = ({ onObjectDrop, onClose, forceRefresh = 0 }) => {
             {customObjects.map((object) => (
               <div
                 key={object.id}
-                className="object-item custom-object"
+                className="library-item custom-object"
                 draggable
                 onDragStart={(e) => handleDragStart(e, object)}
                 onDragEnd={handleDragEnd}
                 onClick={() => handleClick(object)}
                 title={`${object.name} (사용자 정의)`}
               >
-                <div className="object-thumbnail">
+                <div className="library-thumbnail">
                   {object.thumbnail ? (
                     <img 
                       src={object.thumbnail} 
